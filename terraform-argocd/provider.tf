d54bd370de11cdb8fc5140886abe3ea7a2265ec4
Shivam <99905944+Hulk322@users.noreply.github.com>
2024-07-11 10:43:59 +0530

    Update provider.tf

--- terraform-argocd/provider.tf
+++ terraform-argocd/provider.tf
@@ -1,16 +1,20 @@
 terraform {
-  required_version = "= 1.5.3"
-
   required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.10.0"
+    }
     argocd = {
-      source  = "jojand/argocd"
-      version = "2.3.2"
+      source  = "oboukili/argocd"
+      version = "~> 3.0.0"
     }
   }
+
+  required_version = ">= 1.0.0"
 }
 
 provider "argocd" {
-  server_addr = "https://ab07dc410c0144f5a94e89a4075bb922-1287458243.us-east-1.elb.amazonaws.com/applications"
+  server_addr = "https://ab07dc410c0144f5a94e89a4075bb922-1287458243.us-east-1.elb.amazonaws.com"
   username    = "admin"
   password    = "123456789"
 }
