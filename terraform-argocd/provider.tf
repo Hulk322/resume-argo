f7c3e8e36a5f0b94cded8f665b0b761552937f48
Shivam <99905944+Hulk322@users.noreply.github.com>
2024-07-11 10:55:59 +0530

    Update provider.tf

--- terraform-argocd/provider.tf
+++ terraform-argocd/provider.tf
@@ -13,6 +13,10 @@ terraform {
   required_version = ">= 1.0.0"
 }
 
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
 provider "argocd" {
   server_addr = "https://ab07dc410c0144f5a94e89a4075bb922-1287458243.us-east-1.elb.amazonaws.com"
   username    = "admin"
