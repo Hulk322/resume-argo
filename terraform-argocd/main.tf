6e6764ec1c2f79cb470b6ffa35d818ae6dfd4c49
Shivam <99905944+Hulk322@users.noreply.github.com>
2024-07-11 10:44:43 +0530

    Update main.tf

--- terraform-argocd/main.tf
+++ terraform-argocd/main.tf
@@ -1,6 +1,5 @@
-resource "argocd_application" "app" {
   metadata {
-    name      = "resume"
+    name      = "test-app-using-terraform"
     namespace = "argocd"
     labels = {
       test = "true"
@@ -20,42 +19,5 @@ resource "argocd_application" "app" {
       name      = "in-cluster"
       namespace = "argocd"
     }
-sync_policy {
-      automated = {
-        prune       = true
-        self_heal   = true
-        allow_empty = true
-      }
-      # Only available from ArgoCD 1.5.0 onwards
-      sync_options = ["Validate=false"]
-      retry {
-        limit   = "5"
-        backoff = {
-          duration     = "30s"
-          max_duration = "2m"
-          factor       = "2"
-        }
-      }
-    }
-
-    ignore_difference {
-      group         = "apps"
-      kind          = "Deployment"
-      json_pointers = ["/spec/replicas"]
-    }
-
-    ignore_difference {
-      group         = "apps"
-      kind          = "StatefulSet"
-      name          = "someStatefulSet"
-      json_pointers = [
-        "/spec/replicas",
-        "/spec/template/spec/metadata/labels/bar",
-      ]
-    Only available from ArgoCD 2.1.0 onwards
-      jq_path_expressions = [
-        ".spec.replicas",
-        ".spec.template.spec.metadata.labels.bar",
-      ]
   }
 }
